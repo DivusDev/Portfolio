a949da7ebd369c22d6ed1377ca2e06d0c137931c
Tristan Schwichow <tschwichow@gmail.com>
2022-02-06 09:11:38 -0800

    Committed react app

---
+++ client/src/AppRouter.js
@@ -0,0 +1,40 @@
+import {  Route, Routes } from "react-router-dom";
+import Home  from './Components/Pages/Home';
+import { Navbar } from "./Components/Utilities/Navbar";
+
+
+//scss
+import './scss/override.scss'
+import './scss/shapes.scss'
+
+// Components
+
+
+
+
+//HOC for protected routes and location functionality
+const ProtectedRoute  = (Component) => {
+  // setNewActiveTab(rest.location.pathname)
+  return (
+        <>
+          {/* <Navbar  /> */}
+          {Component}
+        </>
+  );
+};
+
+
+
+function AppRouter() {
+  return (
+    <>
+      <Routes>
+          <Route default path="/" element={ProtectedRoute(<Home/>)} />
+          <Route path="about" element={ProtectedRoute(<Home/>)} />
+      </Routes>
+    </>
+    
+  );
+}
+
+export default AppRouter;
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './AppRouter';
+import reportWebVitals from './reportWebVitals';
+
+//Router
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from './AppRouter';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter >
+      <AppRouter />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
